b92197728152a282d9730cd6690404e71a5757e8
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-26 12:52:28 +0200

    some szajt happend

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
-import css from './imageGallery.module.css';
-import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import css from "./imageGallery.module.css"
+import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
+
 
 const ImageGallery = ({ images, onImageClick }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageClick = (largeImageURL) => {
-    setSelectedImage(largeImageURL);
-    onImageClick(largeImageURL);
-  };
-
   return (
-    <div>
-      <ul className={css.gallery}>
-        {images.map((image) => (
-          <ImageGalleryItem
-            key={image.id}
-            image={image}
-            onClick={() => handleImageClick(image.largeImageURL)}
-          />
-        ))}
-      </ul>
-      {selectedImage && (
-        <div className={css.overlay}>
-          <img src={selectedImage} alt="Selected" />
-          <button onClick={() => setSelectedImage(null)}>Close</button>
-        </div>
-      )}
-    </div>
+    <ul className={css.gallery}>
+      {images.map((image) => (
+        <ImageGalleryItem key={image.id} image={image} onClick={() => onImageClick(image.largeImageURL)} />
+      ))}
+    </ul>
   );
 };
 
-export default ImageGallery;
+export default ImageGallery
