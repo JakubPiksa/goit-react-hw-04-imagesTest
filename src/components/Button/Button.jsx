8a5afac994c4a08167bdef93472031306b1a5116
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-26 12:34:45 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,24 @@
-import css from './button.module.css'
+import React, { useState, useEffect } from 'react';
+import css from './button.module.css';
 
 const Button = ({ onClick, isLoading }) => {
+  const [buttonText, setButtonText] = useState('Load more');
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setButtonText(isLoading ? 'Loading...' : 'Load more');
+    setIsDisabled(isLoading);
+  }, [isLoading]);
+
   return (
-      <button
-          className={css.button}
-          onClick={onClick}
-          disabled={isLoading}>
-      {isLoading ? 'Loading...' : 'Load more'}
+    <button
+      className={css.button}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {buttonText}
     </button>
   );
 };
-
 
 export default Button;
