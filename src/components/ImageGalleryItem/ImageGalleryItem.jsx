d8401c513cc149e4dab8901ec47b396908ca5950
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-26 12:39:26 +0200

    tylko te hooki skomplikowały mi się wydaje XD

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './imageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ image, onClick }) => {
+  const [webformatURL, setWebformatURL] = useState('');
+
+  useEffect(() => {
+    setWebformatURL(image.webformatURL);
+  }, [image.webformatURL]);
+
   return (
     <li className={css.galleryItem}>
-      <img src={image.webformatURL} alt="" onClick={onClick} />
+      <img src={webformatURL} alt="" onClick={onClick} />
     </li>
   );
 };
